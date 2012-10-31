90dea42ddfd8b08ec80f245b0ad645cd1c18487c
wangyongle <wangyl@iceflow.cn>
2012-10-31 13:14:17 +0800

    post请求修改完成

--- send_post_request.py
+++ send_post_request.py
@@ -3,23 +3,29 @@
 import httplib, urllib
 import datetime
 import time
+'''
+函数名：send
+作用：给OA发送post请求
+'''
 def send():
-	test_data = {'firstname':'wangyongle'}
-	test_data_urlencode = urllib.urlencode(test_data)
-	requrl="http://172.16.17.101/oa/main/errorindex"
-	headerdata = {"Host":"172.16.17.101"}
-	conn = httplib.HTTPConnection("172.16.17.101")
-	#headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-	conn.request(method="POST",url=requrl,body=test_data_urlencode,headers=headerdata)
-	response = conn.getresponse()
-	data = response.read()
+	test_data = {'name':'zhangqiang',
+			'password':'icflow',
+			'sa':'0',
+			'qwe':'1123132'}
+	test_data_urlencode = urllib.urlencode(test_data)#对数据进行编码
+	requrl="http://172.16.17.101/"#连接地址
+	conn = httplib.HTTPConnection("172.16.17.101")#连接主机
+	headerdata = {"Content-type": "application/x-www-form-urlencoded",
+			 "Accept": "text/plain",
+			"Host":"172.16.17.101"}#post文件头内容
+	conn.request(method="POST",url=requrl,body=test_data_urlencode,headers=headerdata)#发送请求
+	response = conn.getresponse()#获取返回内容对象
+	data = response.read()#从对象中读取数据
 	if data:
-		return True
+		print data
 	else :
 		print 'no data'
-		return False
-	print data
-   	time.sleep(0.1)
+   	time.sleep(0.1)#休息0.1秒后关闭连接
 	conn.close() 
 if __name__ == '__main__':
 	send()
